ff03356cf11d12a76003659bfe8461c56ff885a3
Nehla <47931152+namin1993@users.noreply.github.com>
2026-07-13 03:54:17 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # API Keys and Passwords for the app
 
 # OpenWeather API
-weather_api_key = "155d1a488248f035532e015b69310057"
+weather_api_key = ""
 
 # MongoDB
-username = "namin"
-password =  "passMONG1446778"
-api_key= "wSGoIgyZONBRPGXxkWGdWxpPM77sBVg6"
+username = ""
+password =  ""
+api_key= ""
 
